e181c6e9dcd41d0b1ed8addc89ce0ef6231e724a
angrypie <cookies.in.chocolate@gmail.com>
2019-10-04 16:09:32 +0300

    remove 'g' abbr and use g tool to install latest go version

--- config.fish
+++ config.fish
@@ -7,3 +7,6 @@ end
 
 # Aliases
 alias n=nvim
+abbr -e g
+
+set -gx GOPATH $HOME/go; set -gx GOROOT $HOME/.go; set -gx PATH $GOPATH/bin $PATH; # g-install: do NOT edit, see https://github.com/stefanmaric/g
